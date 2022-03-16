4996e054bd3e2ddc5e674f6e71696907d7d244d5
Momin Haider <h@momin.dev>
2022-03-16 18:38:29 -0400

    Improved tests
    The tests are now working, but the `step()` test needs to be expanded
    upon to ensure the return type is consistent.

--- tests/unit/test_time_series_env.py
+++ tests/unit/test_time_series_env.py
@@ -1,17 +1,23 @@
 import torch
 import unittest
 from environments.time_series_env import TimeSeriesEnv
+from typing import Dict, Tuple
 
 
 class TestTimeSeriesEnv(unittest.TestCase):
     def setUp(self):
-        self.IBM_env = TimeSeriesEnv("IBM")
+        self.num_envs = 4
+        self.IBM_env = TimeSeriesEnv("IBM", self.num_envs, testing_code=True)
 
     def test_should_reset_IBM_env(self):
         self.assertIsInstance(self.IBM_env.reset(), torch.Tensor)
 
     def test_should_step_IBM_env(self):
-        self.IBM_env.step()
+        step_info: Tuple[
+            torch.Tensor, torch.Tensor, torch.Tensor, Dict
+        ] = self.IBM_env.step(
+            torch.rand((self.num_envs,), device=self.IBM_env.device) * 2 - 1
+        )
 
 
 if __name__ == "__main__":
