4fd89bea50232c841b90bc10844ba6c9b0c11947
Momin Haider <h@momin.dev>
2022-03-23 19:39:02 -0400

    Added integration tests to test Isaac Gym setup

---
+++ tests/integration/isaac_gym_instance_helper.py
@@ -0,0 +1,12 @@
+import sys
+from finevol.environments.isaac_gym_env import IsaacGymEnv
+
+
+def create_isaac_vec_environment(env_name: str):
+    isaac_env = IsaacGymEnv(env_name)
+    del isaac_env
+
+
+if __name__ == "__main__":
+    env_name = sys.argv[1]
+    create_isaac_vec_environment(env_name)
---
+++ tests/integration/test_isaac_gym_instantiations.py
@@ -0,0 +1,25 @@
+import isaacgym
+import os
+import unittest
+from finevol.environments.isaac_gym_envs.tasks import isaacgym_task_map
+from subprocess import call
+
+
+class TestIsaacEnvironments(unittest.TestCase):
+    def setUp(self):
+        self.task_map = isaacgym_task_map
+
+    def test_should_instantiate_all_Isaac_vector_environments(self):
+        test_helper_file = os.path.join(
+            os.getcwd(), "tests", "integration", "isaac_gym_instance_helper.py"
+        )
+        for env_name in self.task_map:
+            return_code = call(["python3", test_helper_file, env_name])
+            if return_code != 0:
+                raise Exception(
+                    f"Instantiating {env_name} resulted in error code {return_code}"
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
