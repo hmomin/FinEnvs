9310058be05d65b1dd82c6d4227a10ddf04a7e1c
Momin Haider <h@momin.dev>
2022-03-23 19:38:15 -0400

    Integrated `IsaacGymEnv` for rapid testing

---
+++ finevol/environments/isaac_gym_env.py
@@ -0,0 +1,56 @@
+import isaacgym
+import torch
+from .isaac_gym_envs.tasks import isaacgym_task_map
+from .isaac_gym_envs.tasks.base.vec_task import VecTask
+from .isaac_gym_envs.utils.config_utils import load_task_config
+from typing import Dict, Tuple
+
+
+class IsaacGymEnv:
+    def __init__(
+        self,
+        env_name: str,
+        num_envs: int = -1,
+        headless: bool = False,
+        sim_device_id: int = 0,
+        graphics_device_id: int = 0,
+    ):
+        self.config = load_task_config(env_name)
+        self.set_sim_and_graphics_devices(sim_device_id, graphics_device_id)
+        isaac_task = isaacgym_task_map[env_name]
+        self.override_default_num_envs(num_envs)
+        self.env: VecTask = isaac_task(
+            cfg=self.config,
+            sim_device=self.sim_device,
+            graphics_device_id=self.graphics_device_id,
+            headless=headless,
+        )
+
+    def set_sim_and_graphics_devices(
+        self, sim_device_id: int, graphics_device_id: int
+    ) -> None:
+        if torch.cuda.is_available():
+            self.sim_device = f"cuda:{sim_device_id}"
+            self.graphics_device_id = graphics_device_id
+        else:
+            print("WARNING: PyTorch not recognizing CUDA device -> forcing CPU...")
+            self.sim_device = "cpu"
+            self.graphics_device_id = -1
+
+    def override_default_num_envs(self, num_envs: int) -> None:
+        if num_envs > 0:
+            self.config["env"]["numEnvs"] = num_envs
+
+    def reset(self) -> torch.Tensor:
+        states = self.env.reset()
+        if isinstance(states, Dict):
+            states = states["obs"]
+        assert isinstance(states, torch.Tensor)
+        return states
+
+    def step(
+        self, actions: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, Dict]:
+        (observations_dict, rewards, dones, info_dict) = self.env.step(actions)
+        observations = observations_dict["obs"]
+        return (observations, rewards, dones, info_dict)
---
+++ finevol/environments/isaac_gym_envs/utils/config_utils.py
@@ -0,0 +1,37 @@
+import os
+import yaml
+from ..tasks import isaacgym_task_map
+from typing import Dict
+
+
+def load_task_config(env_name: str) -> Dict:
+    if env_name not in isaacgym_task_map:
+        handle_illegal_environment(env_name)
+    current_dir_name = os.path.dirname(os.path.realpath(__file__))
+    config_root = os.path.join(current_dir_name, "..", "configs")
+    config_filename = os.path.join(config_root, env_name + ".yaml")
+    with open(config_filename) as config_file:
+        task_config = yaml.load(config_file, Loader=yaml.SafeLoader)
+    return task_config
+
+
+def handle_illegal_environment(illegal_name: str):
+    legal_environment_names = ""
+    for env_name in isaacgym_task_map:
+        legal_environment_names += env_name + "\n"
+    raise NameError(
+        f"Incorrect environment name '{illegal_name}' specified for Isaac Gym training.\n"
+        + "Choose from one of the following:\n"
+        + legal_environment_names
+    )
+
+
+def get_isaac_gym_env_args(env_name: str) -> Dict:
+    task_config = load_task_config(env_name)
+    env_config = task_config["env"]
+    return {
+        "env_name": task_config["name"],
+        "num_envs": env_config["numEnvs"],
+        "num_observations": env_config["numObservations"],
+        "num_actions": env_config["numActions"],
+    }
