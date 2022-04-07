e602d28028e8cb556f3939fe3f28a75d18e7af58
Momin Haider <h@momin.dev>
2022-04-07 17:53:30 -0400

    Fixed subtle bug with LSTM implementation
    `LSTMNetwork` had its sequence length and input feature size switched.
    Depending on the environment, both of these parameters will change,
    however.

--- finevol/agents/networks/lstm.py
+++ finevol/agents/networks/lstm.py
@@ -10,7 +10,7 @@ class LSTMNetwork(GenericNetwork):
         output_activation=nn.Identity,
         device_id: int = 0,
     ):
-        super(LSTMNetwork, self).__init__(device_id=device_id)
+        super().__init__(device_id=device_id)
         self.create_layers(shape, output_activation)
 
     def create_layers(self, shape: tuple, output_activation) -> None:
@@ -18,20 +18,21 @@ class LSTMNetwork(GenericNetwork):
             len(shape) == 3
         ), f"LSTM shape expected have length 3, but shape with length {len(shape)} was given"
         (self.input_size, self.hidden_size, self.output_size) = shape
-        self.num_layers = 2
         self.lstm = nn.LSTM(
-            1, self.hidden_size, self.num_layers, batch_first=True, device=self.device
+            self.input_size, self.hidden_size, 1, batch_first=True, device=self.device
         )
         self.last_layer = nn.Sequential(
             nn.Linear(self.hidden_size, self.output_size, device=self.device),
             output_activation(),
         )
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        lstm_inputs = torch.unsqueeze(inputs, dim=-1)
-        # if this is a single batch, we may still need an extra dimension...
-        if len(lstm_inputs.shape) == 2:
+    def forward(self, lstm_inputs: torch.Tensor) -> torch.Tensor:
+        # if this is a single batch, we need an extra dimension...
+        if len(lstm_inputs.shape) == 1:
             lstm_inputs = torch.unsqueeze(lstm_inputs, dim=0)
+        # now, account for the sequence length
+        # FIXME: the sequence length is going to change depending on the environment!
+        lstm_inputs = torch.unsqueeze(lstm_inputs, dim=1)
         lstm_outputs, _ = self.lstm.forward(lstm_inputs)
         # lstm_outputs has shape: (batch_size, sequence_length, hidden_size)
         final_hidden_states = lstm_outputs[:, -1, :]
