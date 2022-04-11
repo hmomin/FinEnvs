f9c95eb9119eaa7827b61ed82625f79ff79695d8
Momin Haider <h@momin.dev>
2022-04-11 08:02:28 -0400

    Converted actor critic tests into MLP tests

--- tests/unit/test_multilayer_perceptron.py
+++ tests/unit/test_multilayer_perceptron.py
@@ -1,6 +1,6 @@
 import torch
 import unittest
-from finevol.agents.networks.multilayer_perceptron import ContinuousActor, Critic
+from finevol.agents.networks.multilayer_perceptron import MLPNetwork
 
 
 class TestMLPNetworks(unittest.TestCase):
@@ -9,30 +9,15 @@ class TestMLPNetworks(unittest.TestCase):
         self.num_envs = 32
         self.num_observations = 24
         self.num_actions = 3
-        self.actor = ContinuousActor(
-            (self.num_observations, 128, 128, self.num_actions),
-        )
-        self.critic = Critic((self.num_observations, 128, 128, 1))
+        self.network = MLPNetwork((self.num_observations, 512, 256, self.num_actions))
         self.states = torch.rand(
-            (self.num_envs, self.num_observations), device=self.actor.device
+            (self.num_envs, self.num_observations), device=self.network.device
         )
 
-    def test_should_feed_forward_actor(self):
-        means = self.actor.forward(self.states)
-        self.assertIsInstance(means, torch.Tensor)
-        self.assertEqual(means.shape, (self.num_envs, self.num_actions))
-
-    def test_should_feed_forward_critic(self):
-        values = self.critic.forward(self.states)
-        self.assertIsInstance(values, torch.Tensor)
-        self.assertEqual(values.shape, (self.num_envs, 1))
-
-    def test_should_get_log_probs_of_actions(self):
-        distribution = self.actor.get_distribution(self.states)
-        actions = distribution.sample()
-        log_probs = distribution.log_prob(actions)
-        self.assertIsInstance(log_probs, torch.Tensor)
-        self.assertEqual(log_probs.shape, (self.num_envs, self.num_actions))
+    def test_should_feed_forward_network(self):
+        outputs = self.network.forward(self.states)
+        self.assertIsInstance(outputs, torch.Tensor)
+        self.assertEqual(outputs.shape, (self.num_envs, self.num_actions))
 
 
 if __name__ == "__main__":
