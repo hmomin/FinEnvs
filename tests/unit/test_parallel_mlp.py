f58653eff39add2fb19634cab4ef81e57c051677
Momin Haider <h@momin.dev>
2022-05-15 10:17:23 -0400

    Reduced sizes of dimensions to speed up tests

--- tests/unit/test_parallel_mlp.py
+++ tests/unit/test_parallel_mlp.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import unittest
 from finevo.agents.networks.parallel_mlp import ParallelMLP
@@ -6,11 +7,13 @@ from finevo.agents.networks.parallel_mlp import ParallelMLP
 class TestParallelMLPNetworks(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.num_envs = 32
+        self.num_envs = 8
         self.num_observations = 24
         self.num_actions = 3
         self.network = ParallelMLP(
-            self.num_envs, (self.num_observations, 512, 256, self.num_actions)
+            self.num_envs,
+            (self.num_observations, 5, 4, self.num_actions),
+            noise_std_dev=0.02,
         )
         self.states = torch.randn(
             (self.num_envs, self.num_observations), device=self.network.device
@@ -30,6 +33,15 @@ class TestParallelMLPNetworks(unittest.TestCase):
 
     def test_4_should_update_parameters(self):
         self.network.update_parameters(self.fitnesses)
+
+    def test_5_should_compute_centered_rank(self):
+        values = torch.tensor(range(31, 26, -1), device=self.network.device)
+        centered_ranks = ParallelMLP.compute_centered_rank(values)
+        true_centered_ranks = torch.tensor(
+            np.arange(0.5, -0.75, -0.25), device=self.network.device
+        )
+        difference = torch.norm(centered_ranks - true_centered_ranks).item()
+        self.assertAlmostEqual(difference, 0)
 
 
 if __name__ == "__main__":
