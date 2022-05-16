ff3a0b43e3299df4d4b74681706cfd2e3ff4eab8
Momin Haider <h@momin.dev>
2022-05-16 10:10:25 -0400

    Sped up tests by removing Isaac Gym instance tests

--- tests/integration/test_isaac_gym_instantiations.py
+++ tests/integration/test_isaac_gym_instantiations.py
@@ -13,12 +13,17 @@ class TestIsaacEnvironments(unittest.TestCase):
         test_helper_file = os.path.join(
             os.getcwd(), "tests", "integration", "isaac_gym_instance_helper.py"
         )
+        # NOTE: Isaac Gym instantiation tests are commented out for now, to speed up
+        # testing. These tests have never failed and take up over 90% of test time.
+        # The IsaacGymEnv module is quite stable at this point.
+        """
         for env_name in self.task_map:
             return_code = call(["python3", test_helper_file, env_name])
             if return_code != 0:
                 raise Exception(
                     f"Instantiating {env_name} resulted in error code {return_code}"
                 )
+        """
 
 
 if __name__ == "__main__":
