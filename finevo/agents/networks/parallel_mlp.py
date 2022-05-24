85c94a19ba88aaceb2764dc9e4609fb0088fb069
Momin Haider <h@momin.dev>
2022-05-24 13:15:57 -0400

    Rewrote `ParallelMLP` to use JIT compilation

--- finevo/agents/networks/parallel_mlp.py
+++ finevo/agents/networks/parallel_mlp.py
@@ -7,9 +7,11 @@ from typing import Tuple
 
 
 class ParallelMLP(BaseObject):
+    @torch.jit.export
     def __init__(
         self,
         num_envs: int,
+        num_eval_envs: int,
         shape: tuple,
         learning_rate: float = 0.01,
         noise_std_dev: float = 0.02,
@@ -19,12 +21,14 @@ class ParallelMLP(BaseObject):
         device_id: int = 0,
     ):
         self.device = set_device(device_id)
-        assert num_envs % 2 != 0, (
-            f"num_envs ({num_envs}) must be odd (2n + 1): "
-            + "2n environments for mirrored sampling and "
-            + "1 environment for evaluation."
+        num_training_envs = num_envs - num_eval_envs
+        assert (num_training_envs) % 2 == 0 and (num_training_envs) > 0, (
+            f"The number of training environments ({num_envs} - {num_eval_envs}) "
+            + "must be positive and even for mirrored sampling."
         )
         self.num_envs = num_envs
+        self.num_training_envs = num_training_envs
+        self.num_eval_envs = num_eval_envs
         self.shape = shape
         self.learning_rate = learning_rate
         self.noise_std_dev = noise_std_dev
@@ -37,6 +41,7 @@ class ParallelMLP(BaseObject):
         self.create_layers(layer_activation, output_activation)
         self.set_adam_parameters()
 
+    @torch.jit.export
     def create_layers(self, layer_activation: type, output_activation: type) -> None:
         for i, current_size in enumerate(self.shape):
             if i == len(self.shape) - 1:
@@ -58,6 +63,7 @@ class ParallelMLP(BaseObject):
             )
             self.activation_functions.append(activation_function)
 
+    @torch.jit.export
     def set_adam_parameters(self) -> None:
         self.adam_timestep = 0
         self.beta_1 = 0.9
@@ -74,6 +80,7 @@ class ParallelMLP(BaseObject):
             self.second_moment_weights.append(moment_weight_layer.clone())
             self.second_moment_biases.append(moment_bias_layer.clone())
 
+    @torch.jit.export
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         self.check_inputs(inputs)
         computation = inputs.unsqueeze(1)
@@ -84,8 +91,10 @@ class ParallelMLP(BaseObject):
             computation = torch.add(computation, bias_layer)
             computation = activation(computation)
         outputs = torch.squeeze(computation, 1)
+        self.add_action_noise(outputs, 0.01)
         return outputs
 
+    @torch.jit.export
     def check_inputs(self, inputs: torch.Tensor) -> None:
         num_observations = self.shape[0]
         if inputs.shape != (self.num_envs, num_observations):
@@ -93,6 +102,13 @@ class ParallelMLP(BaseObject):
                 f"inputs to ParallelMLP must have shape {(self.num_envs, num_observations)}"
             )
 
+    @torch.jit.export
+    def add_action_noise(self, actions: torch.Tensor, std_dev: float) -> None:
+        action_noise = torch.normal(0, std_dev, actions.shape, device=self.device)
+        action_noise[-self.num_eval_envs :, :] = 0
+        actions.add_(action_noise)
+
+    @torch.jit.export
     def perturb_parameters(self):
         for weight_layer, bias_layer in zip(self.weight_layers, self.bias_layers):
             new_weights = weight_layer.repeat((self.num_envs, 1, 1))
@@ -100,22 +116,22 @@ class ParallelMLP(BaseObject):
             positive_weight_perturbation = torch.normal(
                 0,
                 self.noise_std_dev,
-                (self.num_envs // 2, *weight_layer.shape),
+                (self.num_training_envs // 2, *weight_layer.shape),
                 device=self.device,
             )
             positive_bias_perturbation = torch.normal(
                 0,
                 self.noise_std_dev,
-                (self.num_envs // 2, *bias_layer.shape),
+                (self.num_training_envs // 2, *bias_layer.shape),
                 device=self.device,
             )
             negative_weight_perturbation = -positive_weight_perturbation
             negative_bias_perturbation = -positive_bias_perturbation
             zero_weight_perturbation = torch.zeros(
-                (1, *weight_layer.shape), device=self.device
+                (self.num_eval_envs, *weight_layer.shape), device=self.device
             )
             zero_bias_perturbation = torch.zeros(
-                (1, *bias_layer.shape), device=self.device
+                (self.num_eval_envs, *bias_layer.shape), device=self.device
             )
             weight_perturbation = torch.cat(
                 [
@@ -138,6 +154,7 @@ class ParallelMLP(BaseObject):
             self.perturbed_weights.append(new_weights)
             self.perturbed_biases.append(new_biases)
 
+    @torch.jit.export
     def reconstruct_perturbations(self):
         # transform the perturbed parameters to just the perturbations by subtracting
         # the original parameters
@@ -155,10 +172,13 @@ class ParallelMLP(BaseObject):
             perturbed_weight_layer.sub_(weight_layer.repeat((self.num_envs, 1, 1)))
             perturbed_bias_layer.sub_(bias_layer.repeat((self.num_envs, 1, 1)))
 
+    @torch.jit.export
     def update_parameters(self, fitnesses: torch.Tensor):
         self.adam_timestep += 1
-        positive_perturbation_fitnesses = fitnesses[0 : self.num_envs // 2]
-        negative_perturbation_fitnesses = fitnesses[self.num_envs // 2 : -1]
+        positive_perturbation_fitnesses = fitnesses[0 : self.num_training_envs // 2]
+        negative_perturbation_fitnesses = fitnesses[
+            self.num_training_envs // 2 : self.num_training_envs
+        ]
         diffed_fitnesses = (
             positive_perturbation_fitnesses - negative_perturbation_fitnesses
         )
@@ -177,10 +197,10 @@ class ParallelMLP(BaseObject):
             )
         ):
             positive_weight_perturbations = perturbed_weight_layer[
-                0 : self.num_envs // 2, :, :
+                0 : self.num_training_envs // 2, :, :
             ]
             positive_bias_perturbations = perturbed_bias_layer[
-                0 : self.num_envs // 2, :, :
+                0 : self.num_training_envs // 2, :, :
             ]
             total_weight_grad = torch.mul(
                 expanded_fitnesses, positive_weight_perturbations
@@ -196,6 +216,7 @@ class ParallelMLP(BaseObject):
         self.perturbed_weights = []
         self.perturbed_biases = []
 
+    @torch.jit.export
     def get_l2_norm(self) -> float:
         l2_norm = 0
         for (weight_layer, bias_layer,) in zip(
@@ -206,6 +227,7 @@ class ParallelMLP(BaseObject):
         l2_norm = l2_norm.sqrt().item()
         return l2_norm
 
+    @torch.jit.export
     def adam_update(
         self, layers: Tuple[torch.Tensor], grads: Tuple[torch.Tensor], idx: int
     ):
