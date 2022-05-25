971ce54ac52633ab8ff93940854a8eddf37fc3be
Momin Haider <h@momin.dev>
2022-05-25 13:38:23 -0400

    Fixed evolution example to use multiple eval envs

--- examples/evo_MLP_Isaac_Gym.py
+++ examples/evo_MLP_Isaac_Gym.py
@@ -6,17 +6,17 @@ from finevo.environments.isaac_gym_envs.utils.config_utils import (
     get_isaac_gym_env_args,
 )
 
-torch.set_printoptions(sci_mode=False)
 torch.manual_seed(0)
 
 
 def train_Evo_MLP_on_environment(env_name: str):
-    num_envs = 4096 + 1
+    num_envs = 4096
+    num_eval_envs = 12
     episodes_per_batch = 10_000
-    timesteps_per_batch = 10_000_000
 
     env_args = get_isaac_gym_env_args(env_name)
     env_args["num_envs"] = num_envs
+    env_args["num_eval_envs"] = num_eval_envs
 
     env = IsaacGymEnv(env_name, num_envs, headless=True)
     agent = EvoAgent(
@@ -31,12 +31,12 @@ def train_Evo_MLP_on_environment(env_name: str):
     while True:
         actions = agent.step(states)
         (next_states, rewards, dones, _) = env.step(actions)
-        (num_done, timesteps) = agent.store(rewards, dones)
+        (num_done, _) = agent.store(rewards, dones)
         states = next_states
-        if num_done >= episodes_per_batch or timesteps >= timesteps_per_batch:
+        if num_done >= episodes_per_batch:
             agent.train()
             states = env.reset_all()
 
 
 if __name__ == "__main__":
-    train_Evo_MLP_on_environment("Humanoid")
+    train_Evo_MLP_on_environment("Ant")
