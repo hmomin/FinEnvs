15f979bfea44a45d2fbb59cd1bd443b2e1cddae6
Momin Haider <h@momin.dev>
2022-06-10 11:02:16 -0400

    Fixed SPY testing to be conducive to split dataset
    SPY environment testing should allow splitting between train,
    validation, and test datasets.

--- tests/integration/test_SPY_training.py
+++ tests/integration/test_SPY_training.py
@@ -7,18 +7,23 @@ from typing import Dict, Tuple
 class TestSPYTraining(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.num_envs = 4
-        self.env = TimeSeriesEnv("SPY", self.num_envs, testing_code=False)
+        self.num_envs = 1024
+        self.train_env = TimeSeriesEnv("SPY", "train", self.num_envs)
+        # self.valid_env = TimeSeriesEnv("SPY", "valid")
+        # self.test_env = TimeSeriesEnv("SPY", "test")
 
-    def test_should_reset_envs(self):
-        states = self.env.reset()
+    def test_1_should_reset_train_env(self):
+        states = self.train_env.reset()
         self.assertIsInstance(states, torch.Tensor)
 
-    def test_should_step_envs(self):
-        actions = torch.rand((self.num_envs, 1), device=self.env.device) * 2 - 1
-        step_info: Tuple[
-            torch.Tensor, torch.Tensor, torch.Tensor, Dict
-        ] = self.env.step(actions)
+    def test_2_should_step_envs(self):
+        self.step_helper(self.train_env)
+
+    def step_helper(self, env: TimeSeriesEnv):
+        actions = torch.rand((self.num_envs, 1), device=env.device) * 2 - 1
+        step_info: Tuple[torch.Tensor, torch.Tensor, torch.Tensor, Dict] = env.step(
+            actions
+        )
         self.assertIsInstance(step_info, tuple)
         (next_states, rewards, dones, info) = step_info
         self.assertIsInstance(next_states, torch.Tensor)
@@ -26,8 +31,9 @@ class TestSPYTraining(unittest.TestCase):
         self.assertIsInstance(dones, torch.Tensor)
         self.assertIsInstance(info, dict)
 
-    def test_should_step_dataset(self):
-        self.env.step_dataset("train")
+    def test_3_should_step_envs_1000_times(self):
+        for _ in range(1000):
+            self.step_helper(self.train_env)
 
 
 if __name__ == "__main__":
