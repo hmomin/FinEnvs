4d7560348078938ccb1da7d36c314939748ab67e
Momin Haider <h@momin.dev>
2022-06-12 12:15:27 -0400

    Implemented a maximum size for TD3 buffer

--- dev_workspace/TD3/TD3_buffer.py
+++ dev_workspace/TD3/TD3_buffer.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 from finevo.base_object import BaseObject
 from finevo.device_utils import set_device
@@ -6,112 +5,63 @@ from typing import Dict
 
 
 class Buffer(BaseObject):
-    def __init__(
-        self, num_mini_batches: int = 16, gamma: float = 0.99, device_id: int = 0
-    ) -> None:
-        self.num_mini_batches = num_mini_batches
-        self.gamma = gamma
+    def __init__(self, max_size: int = 1_000_000, device_id: int = 0) -> None:
+        self.max_size = max_size
         self.device = set_device(device_id)
         self.container: Dict[str, torch.Tensor] = {
             "states": None,
             "actions": None,
             "rewards": None,
-            "dones": None,
             "next_states": None,
+            "dones": None,
         }
-        self.batch_keys = [
-            "states",
-            "actions",
-            "rewards",
-            "dones",
-            "next_states",
-        ]
 
-    def force_3D(self, tensor: torch.Tensor) -> torch.Tensor:
+    def force_2D(self, tensor: torch.Tensor) -> torch.Tensor:
         if len(tensor.shape) < 2:
             tensor = torch.unsqueeze(tensor, -1)
-        if len(tensor.shape) < 3:
-            tensor = torch.unsqueeze(tensor, 1)
         return tensor
 
     def store_tensor(self, key: str, tensor: torch.Tensor) -> None:
-        tensor = self.force_3D(tensor)
+        tensor = self.force_2D(tensor)
         if self.container[key] == None:
-            self.container[key] = tensor
+            self.container[key] = tensor.float()
         else:
-            self.container[key] = torch.cat([self.container[key], tensor], dim=1)
+            self.container[key] = torch.cat(
+                [self.container[key], tensor], dim=0
+            ).float()
 
     def store(
         self,
         states: torch.Tensor,
         actions: torch.Tensor,
         rewards: torch.Tensor,
-        dones: torch.Tensor,
         next_states: torch.Tensor,
+        dones: torch.Tensor,
     ) -> None:
         self.store_tensor("states", states)
         self.store_tensor("actions", actions)
         self.store_tensor("rewards", rewards)
-        self.store_tensor("dones", dones)
         self.store_tensor("next_states", next_states)
+        self.store_tensor("dones", dones)
+        if self.size() > self.max_size:
+            self.discard_old_data()
 
     def size(self) -> int:
-        states = self.container["states"]
-        if states == None:
+        dones = self.container["dones"]
+        if dones == None:
             return 0
-        elif len(states.shape) == 3:
-            (num_envs, num_steps, num_obs) = states.shape
-            return num_envs * num_steps
         else:
-            (total_steps, num_obs) = states.shape
-            return total_steps
+            return dones.shape[0]
 
-    def prepare_training_data(self) -> None:
-        for key, tensor in self.container.items():
-            (num_envs, num_steps, last_dim) = tensor.shape
-            self.container[key] = torch.reshape(
-                tensor, (num_envs * num_steps, last_dim)
-            )
-
-    def shuffle(self) -> None:
-        with torch.no_grad():
-            random_indices = torch.randperm(
-                self.size(), device=self.device, requires_grad=False
-            )
-            for key, tensor in self.container.items():
-                permuted_tensor = tensor[random_indices, :]
-                self.container[key] = permuted_tensor
-
-    def get_batches(self) -> Dict[str, torch.Tensor]:
-        self.shuffle()
-        batch_dict = {}
-        for key in self.batch_keys:
-            batch_dict[key] = self.container[key]
-        return batch_dict
-
-    def get_mini_batch_indices(self) -> "list[torch.Tensor]":
-        buffer_size = self.size()
-        num_mini_batches = self.num_mini_batches
-        mini_batch_size = int(np.floor(buffer_size / num_mini_batches))
-        if mini_batch_size * num_mini_batches != buffer_size:
-            print(
-                f"WARNING: buffer size {buffer_size} does not divide evenly into {num_mini_batches} mini-batches!"
-            )
-        mini_batch_indices = []
-        for mini_batch_idx in range(num_mini_batches):
-            start_idx = mini_batch_idx * mini_batch_size
-            end_idx = (mini_batch_idx + 1) * mini_batch_size - 1
-            current_mini_batch_indices = torch.linspace(
-                start_idx,
-                end_idx,
-                mini_batch_size,
-                dtype=torch.long,
-                device=self.device,
-                requires_grad=False,
-            )
-            mini_batch_indices.append(current_mini_batch_indices)
-        return mini_batch_indices
-
-    def clear(self) -> None:
+    def discard_old_data(self):
+        assert (
+            self.size() > self.max_size
+        ), "Attempted to discard old values from buffer that's not at max capacity."
         for key in self.container.keys():
-            self.container[key] = None
+            self.container[key] = self.container[key][-self.max_size :, :]
+
+    def get_mini_batch(self, size: int) -> Dict[str, torch.Tensor]:
+        indices = torch.randint(0, self.size(), (size,), device=self.device)
+        keys = self.container.keys()
+        values = self.container.values()
+        return dict([(key, value[indices, :]) for key, value in zip(keys, values)])
