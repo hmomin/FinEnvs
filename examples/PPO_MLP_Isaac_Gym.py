04899af219ff62cdde7a2987df4e0f8cccd0e268
Momin Haider <h@momin.dev>
2022-06-19 09:22:42 -0400

    Ant presets in PPO MLP

--- examples/PPO_MLP_Isaac_Gym.py
+++ examples/PPO_MLP_Isaac_Gym.py
@@ -10,10 +10,16 @@ def train_PPO_MLP_on_environment(env_name: str):
     env_args = get_isaac_gym_env_args(env_name)
     num_envs = env_args["num_envs"]
     batch_size = num_envs * 16
-    max_samples = 1_000_000
+    max_samples = 1_000_000_000
 
     env = IsaacGymEnv(env_name, num_envs, headless=True)
-    agent = PPOAgentMLP(env_args, hidden_dims=(256, 128, 64), write_to_csv=True)
+    agent = PPOAgentMLP(
+        env_args,
+        num_epochs=4,
+        num_mini_batches=4,
+        hidden_dims=(256, 128, 64),
+        write_to_csv=False,
+    )
     states = env.reset()
     total_samples = 0
     while total_samples < max_samples:
@@ -26,4 +32,4 @@ def train_PPO_MLP_on_environment(env_name: str):
 
 
 if __name__ == "__main__":
-    train_PPO_MLP_on_environment("Cartpole")
+    train_PPO_MLP_on_environment("Ant")
