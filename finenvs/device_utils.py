d0ae6bb4ec29089cee7b05bf422e90362e60a263
Momin Haider <h@momin.dev>
2022-06-25 18:29:33 -0400

    Allow a GPU user to switch to CPU

--- finenvs/device_utils.py
+++ finenvs/device_utils.py
@@ -2,7 +2,7 @@ import torch
 
 
 def set_device(device_id: int) -> str:
-    if torch.cuda.is_available():
+    if torch.cuda.is_available() and device_id >= 0:
         return f"cuda:{device_id}"
     else:
         print("WARNING: PyTorch not recognizing CUDA device -> forcing CPU...")
