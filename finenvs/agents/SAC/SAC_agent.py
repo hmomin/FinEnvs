6f0fd065edf873e06b5190871d6dc7b8f46e490a
Momin Haider <h@momin.dev>
2022-06-28 16:54:41 -0400

    Improved `SACAgent`
    - Removed `policy_delay`, since the policy network is updated on every
    step with SAC.
    - Prevented computational graph generation on agent stepping through
    environment.
    - `num_samples` should be updated every time we store, not every time we
    log progress.
    - entropy should be (- alpha * log_probs), not (+ alpha * log_probs),
    since log probabilities are always negative.
    - set starting alpha to 1.0 to encourage exploration in the beginning.

--- finenvs/agents/SAC/SAC_agent.py
+++ finenvs/agents/SAC/SAC_agent.py
@@ -1,10 +1,10 @@
 import csv
 import os
 import torch
-from .actor import Actor, ActorLSTM, ActorMLP
-from .buffer import Buffer
-from .critic import Critic, CriticLSTM, CriticMLP
 from datetime import datetime
+from finenvs.agents.SAC.actor import Actor, ActorLSTM, ActorMLP
+from finenvs.agents.SAC.buffer import Buffer
+from finenvs.agents.SAC.critic import Critic, CriticLSTM, CriticMLP
 from finenvs.agents.networks.generic_network import GenericNetwork
 from finenvs.base_object import BaseObject
 from finenvs.device_utils import set_device
@@ -21,7 +21,6 @@ class SACAgent(BaseObject):
         mini_batch_size: int = 100,
         rho: float = 0.005,
         gamma: float = 0.99,
-        policy_delay: int = 4,
         write_to_csv: bool = True,
         device_id: int = 0,
     ) -> None:
@@ -31,7 +30,6 @@ class SACAgent(BaseObject):
         self.mini_batch_size = mini_batch_size
         self.rho = rho
         self.gamma = gamma
-        self.policy_delay = policy_delay
         self.write_to_csv = write_to_csv
         self.set_network_shapes(hidden_dims)
         self.device = set_device(device_id)
@@ -112,14 +110,13 @@ class SACAgent(BaseObject):
         self,
         states: torch.Tensor,
     ) -> torch.Tensor:
-        dist = self.actor.get_distribution(states)
-        normal_action = dist.rsample()
-        actions = torch.tanh(normal_action).detach()
-        # The last environiment is an "evaluation" environiment, so it should strictly
-        # use the means of the distribution
-        evaluation_state = states[-1]
-        evaluation_action = torch.tanh(self.actor.forward(evaluation_state).detach())
-        actions[-1, :] = evaluation_action
+        with torch.no_grad():
+            dist = self.actor.get_distribution(states)
+            normal_action = dist.rsample()
+            actions = torch.tanh(normal_action).detach()
+            # The last environiment is an "evaluation" environiment, so it should strictly
+            # use the means of the distribution
+            actions[-1, :] = dist.loc[-1, :]
         return actions
 
     def store(
@@ -131,6 +128,7 @@ class SACAgent(BaseObject):
         dones: torch.Tensor,
     ) -> None:
         self.buffer.store(states, actions, rewards, next_states, dones)
+        self.num_samples += self.num_envs
         self.current_returns += rewards
         evaluation_done = dones[-1]
         training_dones = dones[:-1]
@@ -145,7 +143,6 @@ class SACAgent(BaseObject):
         self.current_returns[training_done_indices] = 0
 
     def log_progress(self) -> float:
-        self.num_samples += self.num_envs
         if self.evaluation_return == None:
             return None
         evaluation_return = self.evaluation_return
@@ -177,25 +174,20 @@ class SACAgent(BaseObject):
         return evaluation_return
 
     def train(self) -> Tuple[int, float]:
-        # self.target_log_alpha = self.actor.log_alpha.detach().clone()
         for _ in range(self.num_epochs):
             mini_batch_dict: Dict[str, torch.Tensor] = self.buffer.get_mini_batch(
                 self.mini_batch_size
             )
-            for key in mini_batch_dict.keys():
-                mini_batch_dict[key].requires_grad_()
             states = mini_batch_dict["states"]
             actions = mini_batch_dict["actions"]
             rewards = mini_batch_dict["rewards"]
             next_states = mini_batch_dict["next_states"]
             dones = mini_batch_dict["dones"]
             targets = self.compute_targets(rewards, next_states, dones)
-            self.critic_1.update(states, actions, targets, True)
-            self.critic_2.update(states, actions, targets, True)
-            if self.training_steps % self.policy_delay == 0:
-                self.actor.update(states, self.critic_1, self.critic_2)
-                self.update_target_networks()
-                # self.target_log_alpha = self.actor.log_alpha.detach().clone()
+            self.critic_1.update(states, actions, targets)
+            self.critic_2.update(states, actions, targets)
+            self.actor.update(states, self.critic_1, self.critic_2)
+            self.update_target_networks()
         evaluation_return = self.log_progress()
         self.training_steps += 1
         return (
@@ -221,9 +213,8 @@ class SACAgent(BaseObject):
             min_state_action_value = torch.min(
                 next_state_action_value_1, next_state_action_value_2
             )
-            entropy = self.actor.log_alpha.exp() * log_probs
-            # entropy = self.target_log_alpha.exp()*log_probs
-            target = rewards + (self.gamma) * (1 - dones) * (
+            entropy = -self.actor.log_alpha.exp() * log_probs
+            target = rewards + self.gamma * (1 - dones) * (
                 min_state_action_value - entropy
             )
         return target
@@ -251,9 +242,9 @@ class SACAgentMLP(SACAgent):
         learning_rate: float = 3e-4,
         num_epochs: int = 1,
         mini_batch_size: int = 100,
+        starting_alpha: float = 1.0,
         rho: float = 0.005,
         gamma: float = 0.99,
-        policy_delay: int = 2,
         write_to_csv: bool = True,
         device_id: int = 0,
     ) -> None:
@@ -264,12 +255,13 @@ class SACAgentMLP(SACAgent):
             mini_batch_size,
             rho,
             gamma,
-            policy_delay,
             write_to_csv,
             device_id,
         )
 
-        self.actor = ActorMLP(self.actor_shape, learning_rate, device_id=device_id)
+        self.actor = ActorMLP(
+            self.actor_shape, learning_rate, starting_alpha, device_id=device_id
+        )
         self.critic_1 = CriticMLP(self.critic_shape, learning_rate, device_id=device_id)
         self.critic_2 = CriticMLP(self.critic_shape, learning_rate, device_id=device_id)
         self.target_critic_1 = CriticMLP(
@@ -289,9 +281,9 @@ class SACAgentLSTM(SACAgent):
         learning_rate=3e-4,
         num_epochs: int = 1,
         mini_batch_size: int = 100,
+        starting_alpha: float = 1.0,
         rho: float = 0.005,
         gamma: float = 0.99,
-        policy_delay: int = 2,
         write_to_csv: bool = True,
         device_id: int = 0,
     ) -> None:
@@ -303,12 +295,11 @@ class SACAgentLSTM(SACAgent):
             mini_batch_size,
             rho,
             gamma,
-            policy_delay,
             write_to_csv,
             device_id,
         )
         self.actor = ActorLSTM(
-            self.actor_shape, learning_rate=learning_rate, device_id=device_id
+            self.actor_shape, learning_rate, starting_alpha, device_id=device_id
         )
         self.critic_1 = CriticLSTM(
             self.critic_shape, learning_rate, device_id=device_id
