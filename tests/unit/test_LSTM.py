1d162c0e23a852176c1d7cbf51d8c938d551b3df
Momin Haider <h@momin.dev>
2022-07-05 17:23:31 -0400

    Rewrote LSTM tests to make use of sequence length

--- tests/unit/test_LSTM.py
+++ tests/unit/test_LSTM.py
@@ -8,15 +8,21 @@ class TestLSTMNetworks(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.num_envs = 32
+        self.sequence_length = 5
         self.num_observations = 24
         self.num_actions = 3
-        self.lstm = LSTMNetwork((self.num_observations, 137, self.num_actions), nn.Tanh)
+        self.lstm = LSTMNetwork(
+            (self.num_observations, 137, self.num_actions),
+            self.sequence_length,
+            nn.Tanh,
+        )
         self.states = torch.rand(
-            (self.num_envs, self.num_observations), device=self.lstm.device
+            (self.num_envs, self.sequence_length, self.num_observations),
+            device=self.lstm.device,
         )
 
     def test_should_not_instantiate_lstm_with_more_than_3_layers(self):
-        self.assertRaises(Exception, LSTMNetwork, (1, 2, 3, 4))
+        self.assertRaises(Exception, LSTMNetwork, (1, 2, 3, 4), self.sequence_length)
 
     def test_should_feed_forward_lstm(self):
         outputs = self.lstm.forward(self.states)
