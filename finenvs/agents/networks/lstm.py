624ad4294fc93bb4c564ba4c6a27728f6fa4743d
Momin Haider <h@momin.dev>
2022-07-05 17:21:43 -0400

    Implemented sequence length choice in LSTMs

--- finenvs/agents/networks/lstm.py
+++ finenvs/agents/networks/lstm.py
@@ -7,13 +7,17 @@ class LSTMNetwork(GenericNetwork):
     def __init__(
         self,
         shape: tuple,
+        sequence_length: int,
         output_activation=nn.Identity,
         device_id: int = 0,
     ):
         super().__init__(device_id=device_id)
-        self.create_layers(shape, output_activation)
+        self.create_layers(shape, sequence_length, output_activation)
 
-    def create_layers(self, shape: tuple, output_activation) -> None:
+    def create_layers(
+        self, shape: tuple, sequence_length: int, output_activation
+    ) -> None:
+        self.sequence_length = sequence_length
         if len(shape) == 2:
             # just duplicate the last size as the output size
             shape = tuple([*shape, shape[-1]])
@@ -21,8 +25,13 @@ class LSTMNetwork(GenericNetwork):
             len(shape) == 3
         ), f"LSTM shape expected to have length 3, but shape with length {len(shape)} was given"
         (self.input_size, self.hidden_size, self.output_size) = shape
+        # NOTE: can experiment here with bidirectional and dropout settings
         self.lstm = nn.LSTM(
-            self.input_size, self.hidden_size, 1, batch_first=True, device=self.device
+            self.input_size,
+            self.hidden_size,
+            num_layers=1,
+            batch_first=True,
+            device=self.device,
         )
         self.last_layer = nn.Sequential(
             nn.Linear(self.hidden_size, self.output_size, device=self.device),
@@ -30,12 +39,27 @@ class LSTMNetwork(GenericNetwork):
         )
 
     def forward(self, lstm_inputs: torch.Tensor) -> torch.Tensor:
+        input_shape = lstm_inputs.shape
+        assert (
+            len(input_shape) == 3 or len(input_shape) == 2
+        ), f"LSTM inputs expected to have shape [batch_size (optional), seq_length, input_size]"
+        assert (
+            input_shape[-1] == self.input_size
+        ), f"Last dimension of LSTM inputs expected to be input size ({self.input_size}) - received ({input_shape[-1]})"
+        assert (
+            input_shape[-2] == self.sequence_length
+        ), f"Middle dimension of LSTM inputs expected to be sequence length ({self.sequence_length}) - received ({input_shape[-2]})"
+
+        # FIXME: commenting this out for now
+        """
         # if this is a single batch, we need an extra dimension...
-        if len(lstm_inputs.shape) == 1:
-            lstm_inputs = torch.unsqueeze(lstm_inputs, dim=0)
-        # now, account for the sequence length
-        # FIXME: the sequence length is going to change depending on the environment!
-        lstm_inputs = torch.unsqueeze(lstm_inputs, dim=1)
+        if len(input_shape) == 1:
+            lstm_inputs = lstm_inputs.unsqueeze(0)
+        # the sequence length is going to change depending on the environment!
+        lstm_inputs = lstm_inputs.unsqueeze(1)
+        """
+        # NOTE: lstm_inputs has shape: (batch_size, sequence length, input_size)
+        # in the case of TimeSeriesEnv, it would be something like (num_envs, 390, 4)
         lstm_outputs, _ = self.lstm.forward(lstm_inputs)
         # lstm_outputs has shape: (batch_size, sequence_length, hidden_size)
         final_hidden_states = lstm_outputs[:, -1, :]
